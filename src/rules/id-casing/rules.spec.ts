a6b46c29fd9544a059a904b1ad7c93055e6403f4
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-04-01 14:44:58 +0100

    refactor(rule-tests): colocate test configurations with test-rule utility (#9)

--- src/rules/id-casing/rules.spec.ts
+++ src/rules/id-casing/rules.spec.ts
@@ -1,32 +1,30 @@
-import { beforeAll, describe, expect, it } from 'vitest';
-import { setupTest } from '../../utils/test';
+import { testRule } from '../../utils/test';
 
-const test = setupTest(import.meta.url);
+testRule({
+  ruleName: 'IDCasing',
+  location: import.meta.url,
 
-describe('theme-check/id-casing', () => {
-  beforeAll(async () => {
-    await test.build();
-  });
+  accept: [
+    {
+      description: 'Valid ID',
+      file: 'sections/valid-id.liquid',
+    },
+  ],
 
-  it('reports to theme-check correctly', async () => {
-    const { forConfig, forSection, offenses } = await test.run();
-
-    expect(offenses).toHaveLength(2);
-
-    expect(forConfig()).toHaveLength(1);
-    expect(forSection('invalid-id')).toHaveLength(1);
-    expect(forSection('valid-id')).toHaveLength(0);
-  });
-
-  it('reports errors in the correct location', async () => {
-    const { forConfig, forSection } = await test.run();
-
-    const configError = forConfig()[0];
-    expect(configError.start.index).toBe(107);
-    expect(configError.end.index).toBe(120);
-
-    const invalidIdError = forSection('invalid-id')[0];
-    expect(invalidIdError.start.index).toBe(98);
-    expect(invalidIdError.end.index).toBe(111);
-  });
+  reject: [
+    {
+      description: 'Invalid ID in JSON',
+      file: 'config/settings_schema.json',
+      message: 'Expected ID "giftProduct" to be "gift_product"',
+      startIndex: 107,
+      endIndex: 120,
+    },
+    {
+      description: 'Invalid ID in Liquid',
+      file: 'sections/invalid-id.liquid',
+      message: 'Expected ID "giftProduct" to be "gift_product"',
+      startIndex: 98,
+      endIndex: 111,
+    },
+  ],
 });
--- src/rules/require-image-dimensions/rules.spec.ts
+++ src/rules/require-image-dimensions/rules.spec.ts
@@ -1,74 +1,38 @@
-import { join } from 'node:path';
-import { check } from '@shopify/theme-check-node';
-import { build } from 'tsup';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { testRule } from '../../utils/test';
 
-const __dirname = new URL('.', import.meta.url).pathname;
+testRule({
+  ruleName: 'RequireImageDimensions',
+  location: import.meta.url,
 
-async function runCheck() {
-  const root = join(__dirname, '__fixtures__');
-  const offenses = await check(root, join(root, '.theme-check.yml'));
+  accept: [
+    {
+      description: 'Valid Info Property',
+      file: 'sections/valid-info-property.liquid',
+    },
+  ],
 
-  function forConfig() {
-    return offenses.filter(offense =>
-      offense.absolutePath.endsWith(`/config/settings_schema.json`),
-    );
-  }
-
-  function forSection(filename: string) {
-    return offenses.filter(offense =>
-      offense.absolutePath.endsWith(`/sections/${filename}.liquid`),
-    );
-  }
-
-  return {
-    forConfig,
-    forSection,
-    offenses,
-  };
-}
-
-describe('theme-check/require-image-dimensions', () => {
-  beforeAll(async () => {
-    /**
-     * `console.log` appears to be suppressed whilst this is running.
-     * If you need `console.log`, try running `tsup --watch` to build
-     * the rule in parallel with vitest.
-     */
-    await build({
-      entry: [join(__dirname, 'rules.ts')],
-      outDir: join(__dirname, 'dist'),
-      format: ['cjs'],
-      silent: true,
-    });
-  });
-
-  it('reports to theme-check correctly', async () => {
-    const { forConfig, forSection, offenses } = await runCheck();
-
-    expect(offenses).toHaveLength(3);
-
-    expect(forConfig()).toHaveLength(1);
-    expect(forSection('no-info-property')).toHaveLength(1);
-    expect(forSection('invalid-info-property')).toHaveLength(1);
-    expect(forSection('valid-info-property')).toHaveLength(0);
-  });
-
-  it('reports errors in the correct location', async () => {
-    const { forConfig, forSection } = await runCheck();
-
-    const configError = forConfig()[0];
-    expect(configError.start.index).toBe(52);
-    expect(configError.end.index).toBe(141);
-
-    const sectionNoInfoPropertyError = forSection('no-info-property')[0];
-    expect(sectionNoInfoPropertyError.start.index).toBe(65);
-    expect(sectionNoInfoPropertyError.end.index).toBe(146);
-
-    const sectionInvalidInfoPropertyError = forSection(
-      'invalid-info-property',
-    )[0];
-    expect(sectionInvalidInfoPropertyError.start.index).toBe(153);
-    expect(sectionInvalidInfoPropertyError.end.index).toBe(175);
-  });
+  reject: [
+    {
+      description: 'Invalid Info Property in JSON',
+      file: 'config/settings_schema.json',
+      message: 'Image Picker must contain an "info" property',
+      startIndex: 52,
+      endIndex: 141,
+    },
+    {
+      description: 'Invalid Info Property in Liquid',
+      file: 'sections/invalid-info-property.liquid',
+      message:
+        'The info property must follow this pattern: "/Recommended Size: [\\d]+px x [\\d]+px/"',
+      startIndex: 153,
+      endIndex: 175,
+    },
+    {
+      description: 'No Info Property in Liquid',
+      file: 'sections/no-info-property.liquid',
+      message: 'Image Picker must contain an "info" property',
+      startIndex: 65,
+      endIndex: 146,
+    },
+  ],
 });
